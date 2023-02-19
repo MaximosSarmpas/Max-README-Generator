6614e200f5bb4c096279ed3c0dc50b4c8ceed9f5
MaximosSarmpas <117605647+MaximosSarmpas@users.noreply.github.com>
2023-02-18 23:38:01 -0500

    Update generateMarkdown.js
    some changes was made

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,73 +1,110 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  if (!license) {
-    return '';
-  }
+  if(license === 'MIT License'){
+    return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
+  
+  }else if(license === 'The Unlicense'){
+    return '[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)'
+  
+  }else if(license === 'Apache License 2.0'){
+    '[![License](https://img.shields.io/badge/License-Apache_2.0-yellowgreen.svg)](https://opensource.org/licenses/Apache-2.0)'
+  }}
 
-  const formattedLicense = license.toLowerCase().split(' ').join('-');
-  return `![License](https://img.shields.io/badge/license-${formattedLicense}-brightgreen.svg)`;
-}
 
-// TODO: Create a function that returns the license link
+    // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {
-  if (!license) {
-    return '';
+
+  function renderLicenseLink(license) {
+    if(license === 'no license'){
+      return '';
+    }else{
+      return '\n- [License](#license)'
+    }
+    
   }
 
-  const formattedLicense = license.toLowerCase().split(' ').join('-');
-  return `[${license}](https://choosealicense.com/licenses/${formattedLicense}/)`;
-}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  if (!license) {
-    return '';
+  
+  if (license) {
+    return `## License\nThis product is under the ${license}.\n`
+  } else {
+    return ``;
   }
-
-  return `## License
-
-This project is licensed under the ${renderLicenseLink(license)} license.`;
 }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
+  let license = data.license;
+  let licenseName = "";
+  let badge = "";
   
-  return `# ${data.title} ${renderLicenseBadge(data.license)}
+  if(license === "None")
+    licenseName = "This project has no license";
+  else{
+    licenseName = "This project is licensed under the "+license;
+    
+    switch(license){
+      case "MIT License" : 
+  
+        badge = "https://img.shields.io/badge/license-MIT-green";
+        break;
 
-  ## Description
-  
-  ${data.description}
-  
-  ## Table of Contents
-  
-  ${data.tableOfContents}
-  
-  ## Installation
-  
-  ${data.installation}
-  
-  ## Usage
-  
-  ${data.usage}
-  
-  ${renderLicenseSection(data.license)}
-  
-  ## Contributing
-  
-  ${data.contributing}
-  
-  ## Tests
-  
-  ${data.tests}
-  
-  ## Questions
-  
-  For questions or comments, please contact ${data.author} at ${data.email}.
-  
-  `;
+      case "Apache License 2.0" : 
+        
+        badge = "https://img.shields.io/badge/license-Apache--2.0-blue";
+        break;
+
+      case "GNU General Public License 3.0" : 
+        
+        badge = "https://img.shields.io/badge/license-GPL%203.0-blue";
+        break;
+
+      
+
+      default :
+        badge = "https://img.shields.io/badge/license-MIT-green";
+    }
   }
+  
+
+  return `# ${data.title}
+![Github License](${badge})
+
+
+## Table of Contents
+
+[Description](#description)
+[Installation](#installation)
+[Usage](#usage)
+[Contributing](#contributing)
+[Tests](#tests)
+[Questions](#questions)
+
+## Description
+${data.description}
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## Tests
+${data.test}
+
+## Contributing
+${data.contributing}
+
+
+
+## Questions
+https://github.com/${data.github} 
+Contact me at: ${data.email}`
+}
 
 module.exports = generateMarkdown;
+
